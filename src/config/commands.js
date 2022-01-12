78dee89a9041f559e9a62203f2d2bf13fb58807e
Matheus Madureira <matheus.madureira@g.globo>
2022-01-12 05:33:00 -0300

    fix: bot commands

--- src/config/commands.js
+++ src/config/commands.js
@@ -1,15 +1,16 @@
 import { PREFIX } from "./variables.js";
 
-import play from '../functions/play';
-import skip from '../functions/skip';
-import stop from '../functions/stop';
-import queue from '../functions/queue';
+import play from '../functions/play.js';
+import skip from '../functions/skip.js';
+import stop from '../functions/stop.js';
+import queue from '../functions/queue.js';
 
 const COMMANDS = {
     [`${PREFIX}play`]: (message) => play(message),
     [`${PREFIX}skip`]: (message) => skip(message),
     [`${PREFIX}stop`]: (message) => stop(message),
     [`${PREFIX}queue`]: (message) => queue(message),
+    [`${PREFIX}help`]: (message) => queue(message),
 }
 
 export default COMMANDS;
