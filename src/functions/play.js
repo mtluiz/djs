b6bb057b6bb337dbd310dfc532b30e0d4908bc67
Matheus Madureira <matheus.madureira@g.globo>
2022-01-12 05:33:51 -0300

    feat: atomizing play function

--- src/functions/play.js
+++ src/functions/play.js
@@ -1,68 +1,34 @@
-import { youtubeSearch } from '../utils/youtubeSearch.js';
+import youtubeSearch from '../utils/youtubeSearch.js';
+import { errorCards } from '../config/messages.js';
+import { createQueue, streamSongFromQueue, addSongToQueue} from '../config/queues.js';
 
 export default async function play(message) {
-    const content = message.content.split(' ').shift().join(' ');
+    const content = message.content.split(' ').slice(1).join(' ')
     const voiceChannel = message.member.voice.channel;
+    const permissions = voiceChannel.permissionsFor(message.client.user);
 
-    if (!voiceChannel) {
-        return message.channel.send('mensagem de o usuario estar em algum canal');
+    if (message.content.split(' ').length <= 1) {
+        return message.channel.send({ embeds: [errorCards.typo()] });
     }
 
-    const permission = voiceChannel.permissionsFor(message.client.user);
+    if (!voiceChannel) {
+        return message.channel.send({ embeds: [errorCards.noChannel()] });
+    }
+
 
     if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-        return message.channel.send('mensagem de nao ter permissao');
+        return message.channel.send({ embeds: [errorCards.permission()] });
     }
 
     const searchResult = await youtubeSearch(content);
-
-    const songInfo = await ytdl.getInfo(searchResult);
-
-    /// asdasdasd
-
-    if (!QUEUES_LIST) {
-        const queueContruct = {
-            textChannel: message.channel,
-            voiceChannel: voiceChannel,
-            connection: null,
-            songs: [],
-            volume: 5,
-            playing: true
-        };
-
-        queue.set(message.guild.id, queueContruct);
-
-        queueContruct.songs.push(song);
-
-        try {
-            var connection = await voiceChannel.join();
-            queueContruct.connection = connection;
-            const serverQueue = queue.get(guild.id);
-            if (!song) {
-                serverQueue.voiceChannel.leave();
-                queue.delete(guild.id);
-                return;
-            }
-
-            const dispatcher = serverQueue.connection
-                .play(ytdl(song.url))
-                .on("finish", () => {
-                    serverQueue.songs.shift();
-                    play(guild, serverQueue.songs[0]);
-                })
-                .on("error", error => console.error(error));
-            dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-            serverQueue.textChannel.send(`Tocando agora no Resilia: ${song.title}`);
-        } catch (err) {
-            console.log(err);
-            queue.delete(message.guild.id);
-            return message.channel.send(err);
-        }
-
-    } else {
-        serverQueue.songs.push(song);
-        return message.channel.send(`${song.title} vai tocar a seguir`);
+    const author = {
+        name: message.author.username,
+        icon: `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png`
     }
 
-    /// asdasdasd
+    
+        createQueue(message.guild.id, message.channel, message.member.voice.channel)
+        addSongToQueue(message.guild.id, searchResult, author, message.channel)
+        streamSongFromQueue(message.guild.id, message.member.voice.channel, message)
+    
 }
