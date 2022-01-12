37419d223cb642b0aa7d7769361b6973d80a6e45
Matheus Madureira <matheus.madureira@g.globo>
2022-01-12 05:33:30 -0300

    feat: remaking youtube search function

--- src/utils/youtubeSearch.js
+++ src/utils/youtubeSearch.js
@@ -1,7 +1,17 @@
 import yt from 'yt-search';
+import ytdl from 'ytdl-core';
 
 export default async function youtubeSearch(search) {
-    if (search.includes('http')) return search;
-    const response = await yt(string)
-    return response.all[0].url
+    let url;
+    if (search.includes('https://youtube')) url = search;
+    url = (await yt(search)).all[0].url;
+    const songInfo = await ytdl.getInfo(await url);
+
+    return {
+        title: songInfo.videoDetails.title,
+        duration: Math.floor(songInfo.videoDetails.lengthSeconds / 60) + ' min',
+        thumbnail: songInfo.videoDetails.thumbnails[0].url,
+        url: songInfo.videoDetails.video_url,
+        formats: ytdl.filterFormats(songInfo.formats, 'audioonly')
+    }
 }
